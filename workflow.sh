5cd0cb2d647ecd255dd16f4a15676f8cb1eb6185
Xiaming Chen <chenxm35@gmail.com>
2015-12-13 20:43:39 +0800

    Update workflow

--- workflow.sh
+++ workflow.sh
@@ -16,12 +16,12 @@ source $BASEDIR/global_config.sh
 
 ## Run WifiSyslog cleansing
 chmod +x $BASEDIR/porters/wifi_syslog.sh
-source $BASEDIR/porters/wifi_syslog.sh
+$BASEDIR/porters/wifi_syslog.sh
 
 ## Run WifiSyslogSession extraction
 chmod +x $BASEDIR/porters/wifi_syslog_session.sh
-source $BASEDIR/porters/wifi_syslog_session.sh
+$BASEDIR/porters/wifi_syslog_session.sh
 
 ## Run WifiTrafficHttp cleansing
 chmod +x $BASEDIR/porters/wifi_traffic_http.sh
-source $BASEDIR/porters/wifi_traffic_http.sh
+$BASEDIR/porters/wifi_traffic_http.sh
